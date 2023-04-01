fc1808384c8389623ef9e84d6ea59807ded7e6c2
BlankPeng <925137520@qq.com>
2023-04-01 21:04:50 +0800

    20230401

--- src/com/blank/peng/learn/huawei/Question5.java
+++ src/com/blank/peng/learn/huawei/Question5.java
@@ -1,49 +1,38 @@
 package com.blank.peng.learn.huawei;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Question5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int capacity = scan.nextInt();
+        scan.nextLine();
+        Integer[] files = Arrays.stream(scan.nextLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
 
-    public static void main(String[] args) {//处理输入
-        Scanner in = new Scanner(System.in);
-        String content = in.nextLine();
-        String word = in.nextLine();
-        System.out.println(contin(content, word));
-    }
-
-    public static int contin(String content, String word) {
-
-        HashMap<Character, Integer> contentMap = new HashMap<Character, Integer>();
-        HashMap<Character, Integer> wordMap = new HashMap<Character, Integer>();
-        //先统计出word中的字符组成
-        for (int i = 0; i < word.toCharArray().length; i++)
-            wordMap.put(word.toCharArray()[i], wordMap.getOrDefault(word.toCharArray()[i], 0) + 1);
-
-        char[] contentArr = content.toCharArray();
-        int wordCharKind = wordMap.size();
+        int left = 0;
         int right = 0;
-        int contentChildCharKind = 0;
-        int result = 0;
+        int windownMax = 0;
+        int windownSum = 0;
 
 
-        while (right < content.length()) {
-            if (right >= word.length()) {
-                int left = right - word.length();
-                if (wordMap.containsKey(contentArr[left]) && wordMap.get(contentArr[left]) == contentMap.get(contentArr[left]))
-                    contentChildCharKind -= 1;
-                contentMap.put(contentArr[left], contentMap.getOrDefault(contentArr[left], 0) + 1);
+        while (right < files.length) {
+            int temp = windownSum + files[right];
+            if (temp < capacity) {
+                windownSum += files[right];
+                windownMax = Math.max(windownMax, windownSum);
+                right += 1;
+            } else if (temp > capacity) {
+                windownSum -= files[left];
+                left += 1;
+            } else {
+                System.out.println(capacity);
+                return;
             }
-            contentMap.put(contentArr[right], contentMap.getOrDefault(contentArr[right], 0) + 1);
-            if (wordMap.containsKey(contentArr[right]) && wordMap.get(contentArr[right]) == contentMap.get(contentArr[right])) {
-                contentChildCharKind += 1;
-            }
-            right += 1;
-            if (contentChildCharKind == wordCharKind) {
-                result += 1;
-            }
-
         }
-        return result;
+
+        System.out.println(windownMax);
+
     }
+
 }
