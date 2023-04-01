5e703789fee7c1005ea699f4c8bf0432164d78cf
BlankPeng <925137520@qq.com>
2023-04-01 18:01:59 +0800

    20230401

--- src/com/blank/peng/learn/huawei/Question4.java
+++ src/com/blank/peng/learn/huawei/Question4.java
@@ -1,3 +1,4 @@
+
 package com.blank.peng.learn.huawei;
 
 import java.util.Arrays;
@@ -6,30 +7,60 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Question4 {
-    public static int newWords(String content, String word) {
-        Set<String> result = new HashSet<>();
-        int n = word.length();
-        for (int i = 0; i < content.length() - n + 1; i++) {
-            String substr = content.substring(i, i + n);
-            String sortedSubstr = sortChars(substr);
-            if (sortedSubstr.equals(sortChars(word))) {
-                result.add(substr);
-            }
-        }
-        return result.size();
-    }
-
-    private static String sortChars(String s) {
-        char[] chars = s.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
-    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String content = scanner.nextLine();
-        String word = scanner.nextLine();
-        System.out.println(newWords(content, word));
+        Integer[] area = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+
+        //调研区域的长度
+        int length = area[0];
+        //调研区域的宽度
+        int width = area[1];
+        //电站长、宽
+        int powerStation = area[2];
+        //发电量要求
+        int electricity = area[3];
+
+
+        //初始化二维数组
+        Integer[][] electricityArea = new Integer[2][5];
+
+        for (int i = 0; i < length; i++) {
+            Integer[] temp = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+            for (int j = 0; j < width; j++) {
+                electricityArea[i][j] = temp[j];
+            }
+        }
+
+        System.out.println(getAreaCount(electricityArea, electricity, powerStation));
+
+    }
+
+    private static int getAreaCount(Integer[][] matrix, int threshold, int powerStation) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        //前缀和矩阵
+        int[][] sum = new int[m + 1][n + 1];
+
+        //初始化前缀和矩阵*** 二维数组最后的值为i-1, j -1
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+
+        int answer = 0;
+        for (int i = powerStation; i <= m; i++) {
+            for (int j = powerStation; j <= n; j++) {
+                //获取每平米发电量大于要求的区域
+                if (sum[i][j] - sum[i - powerStation][j] - sum[i][j - powerStation] + sum[i - powerStation][j - powerStation] >= threshold) {
+                    answer += 1;
+                }
+            }
+        }
+
+        return answer;
+
     }
 
 }
